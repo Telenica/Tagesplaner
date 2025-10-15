8874ea5e854daa8b2a354952d76f7a74090c8171
Telenica <jejen560@gmx.de>
2025-10-15 11:56:49 +0200

    Update Planer.py: Menü verbessert

--- Planer.py
+++ Planer.py
@@ -1,39 +1,42 @@
 import sys
 
 def print_start():
-    print("Hallo. Was möchtest du heute tun?")
+    print("Hallo. Was möchtest du tun?")
     print("1 Aufgabe hinzufügen\n2 Aufgabe entfernen\n3 To-Do-Liste ansehen\n4 Exit")
 
 def selection():
     auswahl = ""
     while auswahl == "":
-        print_start
+        print_start()
         try:
-            auswal = int (input("\nWas wollen sie tun?\n"))
+            auswahl = int (input("\nWas wollen sie tun?\n"))
         except:
             print ("Das ist keine Ganzzahl!")
             continue
-        if auswahl < 1 or auswahl > 3:
+        if auswahl < 1 or auswahl > 4:
             print("Keine der vorhandener Menüpunkt")
             auswahl = ""
         else:
             return auswahl
 
 auswahl = selection()
-todolist = {}
+todolist = []
 
 if auswahl == 1:
     name = input("Name der Aufgabe: ")
+    todolist.append(name)
 
-    todolist[name] = input("Aufgabenbeschreibung: ")
     selection()
 
 if auswahl == 2:
-    print("Dieser Menüpunkt ist noch nciht ausgebaut.")
+    name = input("Was möchtest du löschen?")
+    todolist.remove(name)
+    
     selection()
 
 if auswahl == 3:
     print(todolist)
+    
     selection()
 
 if auswahl == 4:
